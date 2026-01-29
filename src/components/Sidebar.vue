79ee75f1a3300e0707a3047cdf2e912087e0249d
Kirito <zhengzhiqiang@geekbuy.com>
2026-01-29 10:29:17 +0800

    init slide bar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,21 +5,33 @@
     background-color="#001529"
     text-color="#fff"
     active-text-color="#409EFF"
+    @select="handleSelect"
   >
-    <el-menu-item index="/dashboard"> 仪表盘 </el-menu-item>
+    <el-menu-item index="/dashboard">仪表盘</el-menu-item>
 
     <el-sub-menu index="system">
       <template #title>系统管理</template>
-      <el-menu-item index="/system/user"> 用户管理 </el-menu-item>
+      <el-menu-item index="/system/user">用户管理</el-menu-item>
     </el-sub-menu>
   </el-menu>
 </template>
 
 <script setup>
-import { useRoute } from "vue-router";
+import { watch, computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { useTabsStore } from "@/store/tabs";
 
 const route = useRoute();
-const activeMenu = route.path;
+const router = useRouter();
+const tabsStore = useTabsStore();
+
+// 侧边栏高亮：绑定 Tabs 活跃的路径
+const activeMenu = computed(() => tabsStore.active);
+
+// 点击左侧菜单 → 路由跳转
+const handleSelect = (index) => {
+  router.push(index);
+};
 </script>
 
 <style scoped>
